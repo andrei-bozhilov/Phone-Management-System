d10be70d159cd0379af174b28c015dcdcfa93055
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2015-03-06 00:40:48 +0200

    Add some more files

---
+++ PhoneManagementSystem.Models/JobTitle.cs
@@ -0,0 +1,24 @@
+﻿namespace PhoneManagementSystem.Models
+{
+    using System.Collections.Generic;
+
+    public class JobTitle
+    {
+        private ICollection<User> users;
+
+        public JobTitle()
+        {
+            this.Users = new HashSet<User>();           
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get { return this.users; }
+            set { this.users = value; }
+        }
+    }
+}
---
+++ PhoneManagementSystem.WebApi/Controllers/JobTitlesController.cs
@@ -0,0 +1,55 @@
+﻿namespace PhoneManagementSystem.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using PhoneManagementSystem.Data;
+
+    [RoutePrefix("api/JobTitles")]
+    public class JobTitlesController : BaseApiController
+    {
+        public JobTitlesController(PhoneSystemData data)
+            : base(data)
+        {
+        }
+
+        public JobTitlesController()
+            : base(new PhoneSystemData())
+        {
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllJobTitles()
+        {
+            var jobTitles = this.Data.JobTitles.All().Select(x => new
+            {
+                id = x.Id,
+                jobTitleName = x.Name
+            }).ToList().OrderBy(x => x.jobTitleName);
+
+            return this.Ok(jobTitles);
+        }
+
+        public IHttpActionResult GetJobTitleById(int id)
+        {
+            var jobTitle = this.Data.JobTitles.All()
+                .Where(x => x.Id == id)
+                .Select(x => new
+                {
+                    id = x.Id,
+                    jobTitleName = x.Name
+                }).FirstOrDefault();
+
+            if (jobTitle == null)
+            {
+                return BadRequest("There is no Job Title with " + id + ".");
+            }
+
+            return this.Ok(jobTitle);
+        }
+    }
+}
